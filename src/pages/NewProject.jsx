58372122601c80453fe7a7b6dddcf96af12141cb
arjunshah276 <arjunshah276@gmail.com>
2025-08-07 15:33:30 -0700

    Add files via upload

---
+++ src/pages/NewProject.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewProject = () => (
+  <div className="p-4">
+    <h1 className="text-2xl font-bold">Create a New Project</h1>
+  </div>
+)
+
+export default NewProject
