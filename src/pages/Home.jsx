51b1a447593086dc93393d513d5c1d42630b865d
arjunshah276 <arjunshah276@gmail.com>
2025-08-07 15:33:13 -0700

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <div className="p-4">
+    <h1 className="text-2xl font-bold">Welcome to the Home Renovation MVP</h1>
+  </div>
+)
+
+export default Home
